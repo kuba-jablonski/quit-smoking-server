49ad4be6ef13dd54b0a3ceac267601bac82dd165
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2018-07-01 15:07:06 +0200

    hash password

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const express = require('express');
 const { User, validate } = require('../models/user');
@@ -11,7 +12,9 @@ router.post('/', async (req, res) => {
   let user = await User.findOne({ email: req.body.email })
   if (user) return res.status(400).send('User already registered.');
 
-  user = new User(_.pick(req.body, ['username', 'email', 'password']))
+  user = new User(_.pick(req.body, ['username', 'email', 'password']));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
   await user.save();
   
