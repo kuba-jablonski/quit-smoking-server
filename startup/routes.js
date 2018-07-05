a2658b5ceb8043c706e0c9afabc2d9bf9d3996ac
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2018-07-05 21:59:05 +0200

    enable cors

--- startup/routes.js
+++ startup/routes.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const helmet = require('helmet')
 const compression = require('compression')
+const cors = require('cors')
 const users = require('../routes/users')
 const auth = require('../routes/auth')
 const error = require('../middleware/error')
 
 module.exports = function (app) {
+  app.all('*', cors())
   app.use(express.json())
   app.use(helmet())
   app.use(compression())
