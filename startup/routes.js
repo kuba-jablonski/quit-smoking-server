9b8890ed9f8a718d7412c4af3b8010143f62b778
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2018-07-07 01:06:40 +0200

    expose x-auth-token header

--- startup/routes.js
+++ startup/routes.js
@@ -7,7 +7,10 @@ const auth = require('../routes/auth')
 const error = require('../middleware/error')
 
 module.exports = function (app) {
-  app.all('*', cors())
+  app.all('*', cors({
+    allowedHeaders: ['Content-Type', 'x-auth-token'],
+    exposedHeaders: ['x-auth-token']
+  }))
   app.use(express.json({ limit: '50mb' }))
   app.use(helmet())
   app.use(compression())
